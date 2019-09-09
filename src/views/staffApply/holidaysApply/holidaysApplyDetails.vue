e06eb874cb63d97f1d745c042799f3f725ee79f8
huanghaidong <80223120@qq.com>
2019-09-10 07:57:02 +0800

    [更新]报销样式

--- src/views/staffApply/holidaysApply/holidaysApplyDetails.vue
+++ src/views/staffApply/holidaysApply/holidaysApplyDetails.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="holidaysApplyDetails">
-    <el-table :data="tableData" stripe>
-      <el-table-column prop="startDate" label="开始时间" width="200"></el-table-column>
-      <el-table-column prop="endDate" label="结束时间" width="200"></el-table-column>
-      <el-table-column prop="typeIdTxt" label="请假类型"></el-table-column>
-      <el-table-column prop="remarks" label="备 注"></el-table-column>
-    </el-table>
+    <el-divider content-position="left">请假明细</el-divider>
+    <div class="view-detail">
+      <el-row :gutter="12">
+        <el-col :span="8">
+          <el-card shadow="always">申请时间：{{holidayItem.createTime}}</el-card>
+        </el-col>
+        <el-col :span="8">
+          <el-card shadow="always">请假天数：{{holidayItem.totalDay}}</el-card>
+        </el-col>
+        <el-col :span="8" v-if="holidayItem.totalAmount != 0">
+          <el-card shadow="always">扣除金额：{{holidayItem.totalAmount}}</el-card>
+        </el-col>
+        <el-col :span="8" v-if="holidayItem.totalAmount != 0">
+          <el-card shadow="always">是否带薪：{{holidayItem.isWithpayTxt}}</el-card>
+        </el-col>        
+        <el-col :span="8" v-if="fileList.length > 0">
+          <el-card shadow="always">文件：
+            <el-link class="margin" icon="el-icon-download"  v-for="(item,key) in fileList" :key="key" @click="openFile(item)">文件{{key+1}}</el-link> 
+          </el-card>
+        </el-col>
+      </el-row>
+      <br />
+      <el-table :data="tableData" stripe>
+        <el-table-column prop="startDate" label="开始时间" width="200"></el-table-column>
+        <el-table-column prop="endDate" label="结束时间" width="200"></el-table-column>
+        <el-table-column prop="typeIdTxt" label="请假类型"></el-table-column>
+        <el-table-column prop="remarks" label="备 注"></el-table-column>
+      </el-table>
+    </div>
+
     <div class="stepSet">
       <el-divider content-position="left">审批流程</el-divider>
         <el-steps :active="step" align-center >
@@ -39,6 +63,7 @@ export default {
       getClaimList: [],
       approveHisList:[],//审批流程
       step:1,
+      fileList:[],
     };
   },
   mounted() {
@@ -50,6 +75,9 @@ export default {
           return item;
       })
       this.holidayItem = this.curInfo;
+      if (this.holidayItem.fileSrc && this.holidayItem.fileSrc != ''){
+        this.fileList = this.holidayItem.fileSrc.split(',');
+      }
       this.tableData = this.curInfo.details.map(item => {
         item.startDate = this.$toolFn.timeFormat(item.startDate);
         item.endDate = this.$toolFn.timeFormat(item.endDate);
@@ -110,6 +138,16 @@ export default {
 </script>
 <style scoped lang="scss">
 .holidaysApplyDetails{
+  .view-detail{
+    width: 95%;
+    margin: 0 auto;
+    .el-card{
+      margin-top:15px;
+      .margin{
+        margin:0 15px
+      }
+    }
+  }
   .my-card{
     p{
       margin-top: 10px;
